6b05760b60fa4f002b9dcf69a7ec51251cd8bad5
hammerhearted <nataran2001@mail.ru>
2021-01-14 12:12:49 +0500

    ,,,

---
+++ MapObjects/Castle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapObjects
+{
+    class Castle
+    {
+        public int Owner { get; set; }
+        public Army Army { get; set; }
+    }
+}
